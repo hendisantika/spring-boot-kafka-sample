6b2555dc8ee9f1b4445b976298a1d43d1c50dbea
hendisantika <hendisantika@yahoo.co.id>
2024-06-12 17:03:42 +0700

    Add JsonProducer in KafkaProducerConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/kafkasample/config/KafkaProducerConfig.java
+++ src/main/java/id/my/hendisantika/kafkasample/config/KafkaProducerConfig.java
@@ -1,7 +1,16 @@
 package id.my.hendisantika.kafkasample.config;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +28,30 @@ public class KafkaProducerConfig {
 
     @Value("${spring.kafka.producer.bootstrap-servers: localhost:9092}")
     private String bootstrapServers;
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // Json Producer
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    @Bean
+    public <T> ProducerFactory<String, T> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.ACKS_CONFIG, "all");
+        configProps.put(ProducerConfig.RETRIES_CONFIG, 3);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        // **___ configure the following three settings for SSL Encryption ___**
+        //configProps.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
+        //configProps.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, "/var/private/ssl/kafka.client.truststore.jks");
+        //configProps.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "test1234");
+
+        // **___configure the following three settings for SSL Authentication ___**
+        //configProps.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, "/var/private/ssl/kafka.client.keystore.jks");
+        //configProps.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, "test1234");
+        //configProps.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, "test1234");
+
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
 }
