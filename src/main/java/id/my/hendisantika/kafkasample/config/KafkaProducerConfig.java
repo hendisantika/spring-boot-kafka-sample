e25f562715658e5a507ee0a9298ca80e6d3bc33a
hendisantika <hendisantika@yahoo.co.id>
2024-06-12 17:03:55 +0700

    Add kafkaTemplate in KafkaProducerConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/kafkasample/config/KafkaProducerConfig.java
+++ src/main/java/id/my/hendisantika/kafkasample/config/KafkaProducerConfig.java
@@ -5,7 +5,9 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
@@ -53,5 +55,11 @@ public class KafkaProducerConfig {
         //configProps.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, "test1234");
 
         return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Primary
+    @Bean
+    public <T> KafkaTemplate<String, T> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 }
