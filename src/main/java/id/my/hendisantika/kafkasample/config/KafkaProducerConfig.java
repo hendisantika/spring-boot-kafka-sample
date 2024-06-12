49976014e54c824a552fa5f7e9a41f986a14eafa
hendisantika <hendisantika@yahoo.co.id>
2024-06-12 17:04:14 +0700

    Add String Producer in KafkaProducerConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/kafkasample/config/KafkaProducerConfig.java
+++ src/main/java/id/my/hendisantika/kafkasample/config/KafkaProducerConfig.java
@@ -62,4 +62,35 @@ public class KafkaProducerConfig {
     public <T> KafkaTemplate<String, T> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // String Producer
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    @Bean
+    public ProducerFactory<String, String> producerStringFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.ACKS_CONFIG, "all");
+        configProps.put(ProducerConfig.RETRIES_CONFIG, 3);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        // **___ configure the following three settings for SSL Encryption ___**
+        //configProps.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
+        //configProps.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, "/var/private/ssl/kafka.client.truststore.jks");
+        //configProps.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "test1234");
+
+        // **___configure the following three settings for SSL Authentication ___**
+        //configProps.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, "/var/private/ssl/kafka.client.keystore.jks");
+        //configProps.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, "test1234");
+        //configProps.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, "test1234");
+
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaStringTemplate() {
+        return new KafkaTemplate<>(producerStringFactory());
+    }
 }
