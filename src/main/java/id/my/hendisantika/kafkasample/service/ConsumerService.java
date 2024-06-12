bc741feeae26657701157acd2cbddd9cb670dc82
hendisantika <hendisantika@yahoo.co.id>
2024-06-12 17:11:24 +0700

    Add ConsumerService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/kafkasample/service/ConsumerService.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.kafkasample.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-kafka-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/12/24
+ * Time: 17:10
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Service
+public class ConsumerService {
+
+}
